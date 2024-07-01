4c4de83eb5d20c49565b779ec323cef44577b28b
achtrick <achrefmtir7@gmail.com>
2024-07-01 13:19:59 +0100

    prod v1

--- pages/reset-password/[token].js
+++ pages/reset-password/[token].js
@@ -84,6 +84,10 @@ function ResetPassword(props) {
                   <label>mot de passe</label>
                   <input
                     required
+                    minLength="8"
+                    maxLength="20"
+                    pattern="^[a-zA-Z0-9]{8,20}$"
+                    title="Le mot de passe doit comporter entre 8 et 20 caractères et contenir uniquement des lettres et des chiffres."
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     className="defaultInput"
