cd71118325bc5402e5b8cea87e36345f57f02b61
achtrick <achrefmtir7@gmail.com>
2024-08-13 10:39:37 +0100

    v1.35

--- middleware.js
+++ middleware.js
@@ -1,27 +1,35 @@
+import { NextResponse } from "next/server";
+
 export async function middleware(request) {
-  // const { pathname } = request.nextUrl;
-  // const hostname = request.headers.get("host");
-  // const url = new URL(request.url);
-  // const domainsJsonUrl = `${url.origin}/domainNames/domainNames.json`;
-  // let basePath = "";
-  // // READ DOMAIN NAME FILE
-  // const domains = await (await fetch(domainsJsonUrl)).json();
-  // // DOMAIN NAMES SECTION
-  // domains.forEach((_) => {
-  //   if (hostname === _.domain || hostname === `www.${_.domain}`) {
-  //     basePath = `/${_.shop}`;
-  //   }
-  // });
-  // // URL REWRITE SECTION
-  // if (basePath && !pathname.startsWith(basePath)) {
-  //   const newurl = new URL(`${basePath}${pathname}`, request.url);
-  //   return NextResponse.rewrite(newurl);
-  // }
-  // return NextResponse.next();
+  const { pathname } = request.nextUrl;
+  const hostname = request.headers.get("host");
+
+  const url = new URL(request.url);
+  const domainsJsonUrl = `${url.origin}/domainNames/domainNames.json`;
+
+  let basePath = "";
+
+  // READ DOMAIN NAME FILE
+  const domains = await (await fetch(domainsJsonUrl)).json();
+
+  // DOMAIN NAMES SECTION
+  domains.forEach((_) => {
+    if (hostname === _.domain || hostname === `www.${_.domain}`) {
+      basePath = `/${_.shop}`;
+    }
+  });
+
+  // URL REWRITE SECTION
+  if (basePath && !pathname.startsWith(basePath)) {
+    const newurl = new URL(`${basePath}${pathname}`, request.url);
+    return NextResponse.rewrite(newurl);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  // matcher: [
-  //   "/((?!api|_next/static|_next/image|favicon.ico|images|manifests).*)",
-  // ],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|images|manifests).*)",
+  ],
 };
